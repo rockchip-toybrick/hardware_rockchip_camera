0edec16afdd23d718a4f0211a0112093cd537335
Wang Panzhenzhuan <randy.wang@rock-chips.com>
2024-01-22 06:16:28 +0000

    psl: fix config pipelineMaxDepth less than 4 preview stuck issue
    when no qbuf before stream on, configs pipelineMaxDepth less than 4
    in camera3_profiles.xml casue preview stuck, fix it.
    
    Type: Fix
    Redmine ID: N/A
    Associated modifications: https://10.10.10.29/c/android/rk/hardware/rk29/camera_v3/+/185636
    Test: camera3_profiles.xml modified request.pipelineMaxDepth value to 2, open camera preview stucked.
    
    Signed-off-by: Wang Panzhenzhuan <randy.wang@rock-chips.com>
    Change-Id: I30c9f6f89367d7012cd19c01f3dd4a85cb5976ab

--- psl/rkisp1/RKISP1CameraHw.cpp
+++ psl/rkisp1/RKISP1CameraHw.cpp
@@ -732,7 +732,6 @@ RKISP1CameraHw::initStaticMetadata(void)
     } else {
         mPipelineDepth = DEFAULT_PIPELINE_DEPTH;
     }
-    mPipelineDepth = HW_PIPELINE_MAX_DEPTH;
 
     /**
      * Check the consistency of the information we had in XML file.
