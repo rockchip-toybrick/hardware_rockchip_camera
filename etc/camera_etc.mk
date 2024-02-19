21f68de7eba52a6a0126c14834ad93263e913bee
Wang Panzhenzhuan <randy.wang@rock-chips.com>
2024-02-19 09:31:20 +0000

    etc: camera_etc.mk: add rk3576 iqfile copy support
    Type: Function
    Redmine ID: N/A
    Associated modifications: N/A
    Test: N/A
    
    Signed-off-by: Wang Panzhenzhuan <randy.wang@rock-chips.com>
    Change-Id: I5a04f40e4b09c3d8dee3f11b5839da6feb1d3cb5

--- etc/camera_etc.mk
+++ etc/camera_etc.mk
@@ -34,6 +34,12 @@ IQ_FILES_PATH := $(TOP)/external/camera_engine_rkaiq/iqfiles/isp32_lite
 PRODUCT_COPY_FILES += \
 	$(call find-copy-subdir-files,*,$(IQ_FILES_PATH)/,$(TARGET_COPY_OUT_VENDOR)/etc/camera/rkisp2/)
 endif
+
+ifneq ($(filter rk3576, $(strip $(TARGET_BOARD_PLATFORM))), )
+IQ_FILES_PATH := $(TOP)/external/camera_engine_rkaiq/iqfiles/isp39
+PRODUCT_COPY_FILES += \
+	$(call find-copy-subdir-files,*,$(IQ_FILES_PATH)/,$(TARGET_COPY_OUT_VENDOR)/etc/camera/rkisp2/)
+endif
 endif
 
 
